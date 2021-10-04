44948e608cb5182af0ac4cbfaae8aed7d82ab9ea
Oliver Epper <oliver.epper@gmail.com>
2021-10-04 14:29:30 +0200

    WIP: reload

--- Schemes/MainViewController.swift
+++ Schemes/MainViewController.swift
@@ -10,22 +10,34 @@ import Cocoa
 @_silgen_name("_LSCopySchemesAndHandlerURLs") func LSCopySchemesAndHandlerURLs(_: UnsafeMutablePointer<NSArray?>, _: UnsafeMutablePointer<NSMutableArray?>) -> OSStatus
 
 class MainViewController: NSViewController {
-    @objc var entries: NSMutableArray {
+    @IBOutlet var arrayController: NSArrayController!
+
+    @objc lazy var entries: NSMutableArray = {
         let entries: NSMutableArray = []
+        loadData(into: entries)
+        return entries
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    private func loadData(into array: NSMutableArray) {
         var s: NSArray?
         var u: NSMutableArray?
         if (LSCopySchemesAndHandlerURLs(&s, &u) != 0) {
             print("ERROR")
-            return entries
+            return
         }
-        guard let schemes = s, let handlerURLs = u else { return entries }
+        guard let schemes = s, let handlerURLs = u else { return }
         for (idx, scheme) in schemes.enumerated() {
-            entries.add(Entry(scheme: scheme as! String, handler: handlerURLs[idx] as! URL))
+            array.add(Entry(scheme: scheme as! String, handler: handlerURLs[idx] as! URL))
         }
-        return entries
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func reloadData(_ sender: Any) {
+        entries.removeAllObjects()
+        loadData(into: entries)
+        arrayController.rearrangeObjects()
     }
 }
